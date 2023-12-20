fc7060c1ce7b834d34260cb33fb6be621bf2ca65
Alexander Willcox <alexander.willcox@students.plymouth.ac.uk>
2023-12-20 14:20:31 +0000

    websocket working

--- Code/back-end/index.js
+++ Code/back-end/index.js
@@ -1,24 +1,38 @@
+
 const express = require("express");
 const app = express();
 const mongoose = require('mongoose');
 const DrinksModel = require('./models/drinks');
 const BasketModel = require('./models/basket');
-
+//const http = require('http');
+//const socketIO = require('socket.io');
+//const server = http.createServer(app);
+//const io = socketIO(server);
+const http = require("http");
+const { Server } = require("socket.io");
 const cors = require("cors");
 
-app.use(express.json());
 app.use(cors());
+
+const server = http.createServer(app);
+
+const io = new Server(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
+});
 
 mongoose.connect("mongodb+srv://alexw123456w:JFeU7ne3L4adSigl@cluster0.2gorhjl.mongodb.net/Drinksdb");
 
 //drink
 app.get("/getDrinks", (req, res) => {
     DrinksModel.find()
-        .then((result)=>{
+        .then((result) => {
             res.status(200).json(result);
         })
-        .catch((error)=>{
-            res.status(500).json(error)    
+        .catch((error) => {
+            res.status(500).json(error)
         })
 });
 
@@ -34,11 +48,11 @@ app.post("/createDrink", async (req, res) => {
 //basket
 app.get("/getBasket", (req, res) => {
     BasketModel.find()
-        .then((result)=>{
+        .then((result) => {
             res.status(200).json(result);
         })
-        .catch((error)=>{
-            res.status(500).json(error)    
+        .catch((error) => {
+            res.status(500).json(error)
         })
 });
 
@@ -51,9 +65,21 @@ app.post("/addBasket", async (req, res) => {
 
 });
 
+io.on("connection", (socket) => {
+  console.log(`User Connected: ${socket.id}`);
 
-  
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
 
-app.listen(3001, () => {
-    console.log("Server Running");
+  socket.on("send_message", (data) => {
+    socket.to(data.room).emit("receive_message", data);
+  });
 });
+
+server.listen(3001, () => {
+  console.log("SERVER IS RUNNING");
+});
+
+
+
